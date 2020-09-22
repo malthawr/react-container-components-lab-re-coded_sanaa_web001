be7064ccd440a11e77ce362c427af20444444d87
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-22 08:28:55 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,20 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const rev = ({headline,byline,summary_short}) => {
+  return (
+    <div className="review">
+
+          <h1>{headline}</h1>
+        <p className="author">{byline}</p>
+    </div>
+  );
+};
+
+const MovieReviews = ({ reviews }) => {
+  return(
+    <div className="review-list"> { reviews.map(rev)} </div>
+  );
+}
+
+export default MovieReviews
